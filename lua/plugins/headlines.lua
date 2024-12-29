f7f13d166786a3fb5b089cea93a4a85a6efc0e7e
George <obsidian.musicwork@gmail.com>
2024-12-29 09:18:35 +0300

    Update plugins

--- lua/plugins/headlines.lua
+++
@@ -1,9 +0,0 @@
--- This plugin adds horizontal highlights for text filetypes, like markdown, orgmode, and neorg
-
-return {
-  "lukas-reineke/headlines.nvim",
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  enabled = false,
-  lazy = true,
-  opts = {},
-}
