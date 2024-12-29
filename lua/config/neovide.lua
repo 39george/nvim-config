d82780040191c2e6b82956f896a048a2a2b3fcd5
George <obsidian.musicwork@gmail.com>
2024-12-29 11:47:09 +0300

    Remove unused config file

--- lua/config/neovide.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.neovide == true then
-  -- stylua: ignore start
-  vim.api.nvim_set_keymap( "n", "<F11>", ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
-  vim.o.guifont = "JetBrains Mono:h12:#e-subpixelantialias:#h-none"
-  -- stylua: ignore end
-end
-
