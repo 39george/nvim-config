8ee5cfec65a02263237d4d03a5fa23ca242adf26
George <obsidian.musicwork@gmail.com>
2024-12-31 10:21:34 +0300

    Add custom colorschemes

---
+++ lua/colorschemes/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    dir = "~/.config/nvim/lua/colorschemes/darcula",
+  },
+  {
+    dir = "~/.config/nvim/lua/colorschemes/patana",
+  },
+  {
+    dir = "~/.config/nvim/lua/colorschemes/pinky",
+    -- enabled = false,
+  },
+}
