fe1baefb5747faad8755c42056cc85879d25cd64
George <obsidian.musicwork@gmail.com>
2025-01-01 14:08:39 +0300

    Add lsp snippets support for lua and go

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -16,6 +16,26 @@ return {
           settings = {
             gopls = {
               gofumpt = true,
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              semanticTokens = true,
             },
           },
         },
@@ -71,7 +91,13 @@ return {
           -- single_file_support = true,
           -- log_level = vim.lsp.protocol.MessageType.Warning,
           settings = {
-            Lua = {},
+            Lua = {
+              completion = {
+                callSnippet = "Replace",
+                keywordSnippet = "Replace",
+                displayContext = 5, -- Display 5 lines of completion context
+              },
+            },
           },
         },
         bashls = {
