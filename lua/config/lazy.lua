06a96af01d4ceeffe46f9df9c99629f171ce9f88
George <obsidian.musicwork@gmail.com>
2025-01-02 13:37:57 +0300

    Use treesitter based folding instead

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,7 +37,15 @@ vim.g.maplocalleader = "\\"
 
 -- General options
 
+vim.o.foldcolumn = "1"
 vim.opt.scrolloff = 5
+vim.o.foldtext = ""
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 vim.cmd("set termguicolors")
 vim.cmd("set number")
