9d1bfa32ca0e601a0454d6ba111970afc1450396
George <obsidian.musicwork@gmail.com>
2025-01-03 16:07:52 +0300

    Add yank highlight

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("config.icons")
 require("config.lazy")
 vim.cmd("colorscheme darcula")
+
+-- Highlight text on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*", -- Apply to all buffers
+  callback = function()
+    require("mini.cursorword").setup()
+    vim.highlight.on_yank({ higroup = "Yank", timeout = 300 })
+  end,
+  desc = "Highlight text after yanking", -- Description of the autocommand
+})
