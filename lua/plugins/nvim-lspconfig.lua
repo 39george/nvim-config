e54d46dbe7079e49a150576dbd3c6a02fe6d776f
George <obsidian.musicwork@gmail.com>
2025-01-06 13:19:16 +0300

    Add clangd support

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,29 @@
 -- Default Nvim LSP client configurations for various LSP servers.
 
+local function switch_source_header(bufnr)
+  local method_name = "textDocument/switchSourceHeader"
+  bufnr = util.validate_bufnr(bufnr)
+  local client = util.get_active_client_by_name(bufnr, "clangd")
+  if not client then
+    return vim.notify(
+      ("method %s is not supported by any servers active on the current buffer"):format(
+        method_name
+      )
+    )
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  client.request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify("corresponding file cannot be determined")
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
 return {
   "neovim/nvim-lspconfig", -- Add LSP config
   dependencies = { "saghen/blink.cmp" },
@@ -141,6 +165,106 @@ return {
             ".git"
           ),
           single_file_support = true,
+        },
+        clangd = {
+          cmd = { "clangd" },
+          filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+          root_dir = function(fname)
+            return util.root_pattern(
+              ".clangd",
+              ".clang-tidy",
+              ".clang-format",
+              "compile_commands.json",
+              "compile_flags.txt",
+              "configure.ac" -- AutoTools
+            )(fname) or vim.fs.dirname(
+              vim.fs.find(".git", { path = fname, upward = true })[1]
+            )
+          end,
+          single_file_support = true,
+          capabilities = {
+            textDocument = {
+              completion = {
+                editsNearCursor = true,
+              },
+            },
+            offsetEncoding = { "utf-8", "utf-16" },
+          },
+        },
+        commands = {
+          ClangdSwitchSourceHeader = {
+            function()
+              local util = require("lspconfig.util")
+              local bufnr = 0
+              local method_name = "textDocument/switchSourceHeader"
+              bufnr = util.validate_bufnr(bufnr)
+              local client = util.get_active_client_by_name(bufnr, "clangd")
+              if not client then
+                return vim.notify(
+                  ("method %s is not supported by any servers active on the current buffer"):format(
+                    method_name
+                  )
+                )
+              end
+              local params = vim.lsp.util.make_text_document_params(bufnr)
+              client.request(method_name, params, function(err, result)
+                if err then
+                  error(tostring(err))
+                end
+                if not result then
+                  vim.notify("corresponding file cannot be determined")
+                  return
+                end
+                vim.cmd.edit(vim.uri_to_fname(result))
+              end, bufnr)
+            end,
+            description = "Switch between source/header",
+          },
+          ClangdShowSymbolInfo = {
+            function()
+              local util = require("lspconfig.util")
+              local bufnr = vim.api.nvim_get_current_buf()
+              local clangd_client =
+                util.get_active_client_by_name(bufnr, "clangd")
+              if
+                not clangd_client
+                or not clangd_client.supports_method("textDocument/symbolInfo")
+              then
+                return vim.notify(
+                  "Clangd client not found",
+                  vim.log.levels.ERROR
+                )
+              end
+              local win = vim.api.nvim_get_current_win()
+              local params = vim.lsp.util.make_position_params(
+                win,
+                clangd_client.offset_encoding
+              )
+              clangd_client.request(
+                "textDocument/symbolInfo",
+                params,
+                function(err, res)
+                  if err or #res == 0 then
+                    -- Clangd always returns an error, there is not reason to parse it
+                    return
+                  end
+                  local container =
+                    string.format("container: %s", res[1].containerName) ---@type string
+                  local name = string.format("name: %s", res[1].name) ---@type string
+                  vim.lsp.util.open_floating_preview({ name, container }, "", {
+                    height = 2,
+                    width = math.max(string.len(name), string.len(container)),
+                    focusable = false,
+                    focus = false,
+                    border = "single",
+                    title = "Symbol Info",
+                  })
+                end,
+                bufnr
+              )
+            end,
+            description = "Show symbol info",
+          },
         },
       },
     }
