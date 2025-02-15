13c745097b4cd6de8ac51dbd1b478f0fdb8ec9d9
39george <obsidian.musicwork@gmail.com>
2025-02-15 15:57:38 +0300

    feat(rust): add rustaceanvim configuration

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,3 +1,28 @@
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {},
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          buildScripts = {
+            enable = true,
+          },
+        },
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {},
+}
+
 return {
   "mrcjkb/rustaceanvim",
 }
