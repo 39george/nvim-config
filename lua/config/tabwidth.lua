bfd532aabe80a18c5b7c14b015a076c8c85417d7
39george <obsidian.musicwork@gmail.com>
2025-04-17 19:59:37 +0300

    feat(plugins): update configs

--- lua/config/tabwidth.lua
+++ lua/config/tabwidth.lua
@@ -1,4 +1,5 @@
 vim.cmd([[autocmd FileType lua setlocal shiftwidth=2 tabstop=2 expandtab]])
 vim.cmd([[autocmd FileType sql setlocal shiftwidth=2 tabstop=2 expandtab]])
 vim.cmd([[autocmd FileType json setlocal shiftwidth=2 tabstop=2 expandtab]])
+vim.cmd([[autocmd FileType javascript setlocal shiftwidth=2 tabstop=2 expandtab]])
 vim.cmd([[autocmd FileType proto setlocal shiftwidth=4 tabstop=4 expandtab]])
