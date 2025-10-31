e4192e78fdc3546d71f20971650418883f99e97f
39george <obsidian.musicwork@gmail.com>
2025-10-31 12:13:19 +0300

    feat: replace dbee to dadbod

---
+++ lua/plugins/vim-dadbod.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true,
+      },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", ":DBUIToggle<CR>", mode = "n", desc = "Toggle dbui", },
+    },
+  },
+  { -- optional saghen/blink.cmp completion source
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        -- add vim-dadbod-completion to your completion providers
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+  },
+}
