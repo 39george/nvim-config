f184ffe3f87e6d27c42de1f41316f491cf3d5ef9
39george <obsidian.musicwork@gmail.com>
2025-12-28 10:57:20 +0300

    feat(colorscheme): use nordic

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
-if vim.fn.has("win32") then
-  vim.g.loaded_man = 1
-end
+if vim.fn.has("win32") then vim.g.loaded_man = 1 end
 
 require("config.icons")
 require("config.lazy")
-vim.cmd("colorscheme darcula")
+vim.cmd("colorscheme nordic")
+-- vim.cmd("colorscheme darcula")
