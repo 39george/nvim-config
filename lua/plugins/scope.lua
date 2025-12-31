8f6b7a7d1f3c92cfa5f009466b7c68b61993cc5d
39george <obsidian.musicwork@gmail.com>
2025-12-31 05:34:46 +0300

    return back

--- lua/plugins/scope.lua
+++ lua/plugins/scope.lua
@@ -3,14 +3,9 @@ return {
   opts = {
     hooks = {
       pre_tab_leave = function()
-        local buf = vim.api.nvim_get_current_buf()
-        if vim.bo[buf].filetype == "snacks_dashboard" then
-          -- Important: Do not close dashboard in TabLeave
-          vim.schedule(function()
-            pcall(function() require("snacks").dashboard.close() end)
-          end)
+        if vim.bo.filetype == "snacks_dashboard" then
+          pcall(function() require("snacks").dashboard.close() end)
         end
-        vim.api.nvim_exec_autocmds("User", { pattern = "ScopeTabLeavePre" })
       end,
 
       post_tab_enter = function()
