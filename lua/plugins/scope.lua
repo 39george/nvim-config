b3c6650c2272c3b6cf5766f3c9fcc1040a78f511
39george <obsidian.musicwork@gmail.com>
2025-12-31 05:31:10 +0300

    patch: fix dashboard + scope issue

--- lua/plugins/scope.lua
+++ lua/plugins/scope.lua
@@ -3,8 +3,12 @@ return {
   opts = {
     hooks = {
       pre_tab_leave = function()
-        if vim.bo.filetype == "snacks_dashboard" then
-          pcall(function() require("snacks").dashboard.close() end)
+        local buf = vim.api.nvim_get_current_buf()
+        if vim.bo[buf].filetype == "snacks_dashboard" then
+          -- Important: Do not close dashboard in TabLeave
+          vim.schedule(function()
+            pcall(function() require("snacks").dashboard.close() end)
+          end)
         end
         vim.api.nvim_exec_autocmds("User", { pattern = "ScopeTabLeavePre" })
       end,
