45ed4e7c6520db73a94025c0dec3dc12db067210
39george <obsidian.musicwork@gmail.com>
2026-01-02 07:23:20 +0300

    feat: make Outline lazy

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,8 +1,7 @@
 return {
   "hedyhli/outline.nvim",
-  config = function()
-    -- stylua: ignore
-    vim.keymap.set( "n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-    require("outline").setup({})
-  end,
+  keys = {
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle Outline" },
+  },
+  opts = {},
 }
